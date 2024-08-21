9604d160244b0910ba03315cd4e8638f192f5adf
Bhartijadaun <bhartisinghjadoun@gmail.com>
2024-08-21 15:42:12 +0530

    commit

---
+++ src/exe_20082024/Lab006.py
@@ -0,0 +1,15 @@
+def Fizz_Buzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+Fizz_Buzz()
+
+
+
